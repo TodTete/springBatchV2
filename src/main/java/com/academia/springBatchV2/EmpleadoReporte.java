cf1af6906ba3efd48f7c3a71ad96d27ce3a6cab9
TodTete <vallejoricardo3@gmail.com>
2026-02-23 09:23:00 -0600

    empleado report

--- src/main/java/com/academia/springBatchV2/EmpleadoReporte.java
+++ src/main/java/com/academia/springBatchV2/EmpleadoReporte.java
@@ -1,13 +1,61 @@
 package com.academia.springBatchV2;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+public class EmpleadoReporte {
 
-@SpringBootApplication
-public class SpringBatchV2Application {
+	private String nombre;
+	private String departamento;
+	private double salario;
+	private double bono;
+	private double salarioTotal;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBatchV2Application.class, args);
+	public EmpleadoReporte() {
 	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public double getBono() {
+		return bono;
+	}
+
+	public void setBono(double bono) {
+		this.bono = bono;
+	}
+
+	public double getSalarioTotal() {
+		return salarioTotal;
+	}
+
+	public void setSalarioTotal(double salarioTotal) {
+		this.salarioTotal = salarioTotal;
+	}
+
+	@Override
+	public String toString() {
+		return nombre + " | " + departamento +
+				" | Salario: " + salario +
+				" | Bono: " + bono +
+				" | Total: " + salarioTotal;
+	}
 }
