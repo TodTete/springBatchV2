c66c8963827b07959e8a04d3578b035aa5b78daf
TodTete <vallejoricardo3@gmail.com>
2026-02-23 09:14:37 -0600

    Estructura del proyecto

---
+++ src/main/java/com/academia/springBatchV2/Empleado.java
@@ -0,0 +1,13 @@
+package com.academia.springBatchV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchV2Application.class, args);
+	}
+
+}
