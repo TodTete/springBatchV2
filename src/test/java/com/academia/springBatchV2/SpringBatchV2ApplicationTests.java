5fdbf957ab561806c9fb870df2d0d844e1d33e9b
TodTete <vallejoricardo3@gmail.com>
2026-02-23 09:05:53 -0600

    Inicializacion del proyecto

---
+++ src/test/java/com/academia/springBatchV2/SpringBatchV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academia.springBatchV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
